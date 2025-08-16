3a36b02a9be704c1971461d8587021903e2af2b7
chinesepowered <nlai@rediffmail.com>
2025-08-15 18:39:42 -0700

    examples

---
+++ examples/audio-to-audio.tsx
@@ -0,0 +1,102 @@
+// Test file: https://storage.googleapis.com/generativeai-downloads/data/16000.wav
+// Install helpers for converting files: npm install wavefile
+import { GoogleGenAI, Modality } from '@google/genai';
+import * as fs from "node:fs";
+import pkg from 'wavefile';
+const { WaveFile } = pkg;
+
+const ai = new GoogleGenAI({});
+const model = 'gemini-live-2.5-flash-preview';
+const config = { responseModalities: [Modality.TEXT] };
+
+async function live() {
+  const responseQueue = [];
+
+  async function waitMessage() {
+    let done = false;
+    let message = undefined;
+    while (!done) {
+      message = responseQueue.shift();
+      if (message) {
+        done = true;
+      } else {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    }
+    return message;
+  }
+
+  async function handleTurn() {
+    const turns = [];
+    let done = false;
+    while (!done) {
+      const message = await waitMessage();
+      turns.push(message);
+      if (message.serverContent && message.serverContent.turnComplete) {
+        done = true;
+      }
+    }
+    return turns;
+  }
+
+  const session = await ai.live.connect({
+    model: model,
+    callbacks: {
+      onopen: function () {
+        console.debug('Opened');
+      },
+      onmessage: function (message) {
+        responseQueue.push(message);
+      },
+      onerror: function (e) {
+        console.debug('Error:', e.message);
+      },
+      onclose: function (e) {
+        console.debug('Close:', e.reason);
+      },
+    },
+    config: config,
+  });
+
+  // Send Audio Chunk
+  const fileBuffer = fs.readFileSync("sample.wav");
+
+  // Ensure audio conforms to API requirements (16-bit PCM, 16kHz, mono)
+  const wav = new WaveFile();
+  wav.fromBuffer(fileBuffer);
+  wav.toSampleRate(16000);
+  wav.toBitDepth("16");
+  const base64Audio = wav.toBase64();
+
+  // If already in correct format, you can use this:
+  // const fileBuffer = fs.readFileSync("sample.pcm");
+  // const base64Audio = Buffer.from(fileBuffer).toString('base64');
+
+  session.sendRealtimeInput(
+    {
+      audio: {
+        data: base64Audio,
+        mimeType: "audio/pcm;rate=16000"
+      }
+    }
+
+  );
+
+  const turns = await handleTurn();
+  for (const turn of turns) {
+    if (turn.text) {
+      console.debug('Received text: %s\n', turn.text);
+    }
+    else if (turn.data) {
+      console.debug('Received inline data: %s\n', turn.data);
+    }
+  }
+
+  session.close();
+}
+
+async function main() {
+  await live().catch((e) => console.error('got error', e));
+}
+
+main();
